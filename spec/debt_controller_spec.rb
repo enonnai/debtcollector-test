efcbec489ff2a56f59c473e2d5824d65baa1bd47
Cristina Iannone <iannonecristina@gmail.com>
2017-07-12 22:59:02 +0100

    Add 'VAT rate equal to 0' test scenario

--- spec/debt_controller_spec.rb
+++ spec/debt_controller_spec.rb
@@ -2,10 +2,21 @@ require "debt_collector"
 
 describe DebtCollector do
 
-  describe "#display_formatted_output" do
-    it "returns a string containing a company name followed by their debt amount in pounds including VAT" do
-      subject = DebtCollector.new("Company name" => "Schmidt Inc", "Invoice number" => "INV-001", "Amount in pence excl vat" => 592499005, "VAT Rate" => 20)
-      expect(subject.display_formatted_output).to eq "Schmidt Inc: £7,109,988.06 incl. VAT"
+  context 'VAT rate is equal to 20' do
+    describe "#display_formatted_output" do
+      it "returns a string containing a company name followed by their debt amount in pounds including VAT" do
+        subject = DebtCollector.new("Company name" => "Schmidt Inc", "Invoice number" => "INV-001", "Amount in pence excl vat" => 592499005, "VAT Rate" => 20)
+        expect(subject.display_formatted_output).to eq "Schmidt Inc: £7,109,988.06 incl. VAT"
+      end
+    end
+  end
+
+  context 'VAT rate is equal to 0' do
+    describe "#display_formatted_output" do
+      it "returns a string containing a company name followed by their debt amount in pounds including VAT" do
+        subject = DebtCollector.new("Company name" => "Lebsack Inc", "Invoice number" => "INV-765", "Amount in pence excl vat" => 32780817, "VAT Rate" => 0)
+        expect(subject.display_formatted_output).to eq "Lebsack Inc: £327,808.17 incl. VAT"
+      end
     end
   end
 
